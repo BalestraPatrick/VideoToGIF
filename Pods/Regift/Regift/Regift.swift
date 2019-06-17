1b289a4b6576c5af826703f42f9f742c8cdece62
Tiago Canto <tiago.canto01@gmail.com>
2019-06-17 10:22:36 -0300

    Conversion to Swift 5

--- Pods/Regift/Regift/Regift.swift
+++ Pods/Regift/Regift/Regift.swift
@@ -233,7 +233,7 @@ public struct Regift {
         
         for frameNumber in 0 ..< frameCount {
             let seconds: Float64 = Float64(startTime) + (Float64(increment) * Float64(frameNumber))
-            let time = CMTimeMakeWithSeconds(seconds, Constants.TimeInterval)
+            let time = CMTimeMakeWithSeconds(seconds, preferredTimescale: Constants.TimeInterval)
             
             timePoints.append(time)
         }
@@ -281,7 +281,7 @@ public struct Regift {
         
         generator.appliesPreferredTrackTransform = true
         
-        let tolerance = CMTimeMakeWithSeconds(Constants.Tolerance, Constants.TimeInterval)
+        let tolerance = CMTimeMakeWithSeconds(Constants.Tolerance, preferredTimescale: Constants.TimeInterval)
         generator.requestedTimeToleranceBefore = tolerance
         generator.requestedTimeToleranceAfter = tolerance
 
